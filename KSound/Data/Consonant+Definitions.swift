c07d59ead5a8d93b44d65f0ab55d3031f9c1e8f3
Christopher Fonseka <chris.fonseka@googlemail.com>
2020-11-08 19:30:51 +0000

    Added data model for consonants

---
+++ KSound/Data/Consonant+Definitions.swift
@@ -0,0 +1,16 @@
+//
+//  Consonant+Definitions.swift
+//  KSound
+//
+//  Created by Christopher Fonseka on 08/11/2020.
+//
+
+import Foundation
+
+extension Consonoant {
+    static var all: [Consonoant] = [
+        .init(character: "\u{1100}", style: .basic),
+        .init(character: "\u{1102}", style: .basic),
+        .init(character: "\u{1103}", style: .basic)
+    ]
+}
---
+++ KSound/Data/Consonant.swift
@@ -0,0 +1,38 @@
+//
+//  Consonant.swift
+//  KSound
+//
+//  Created by Christopher Fonseka on 08/11/2020.
+//
+
+import Foundation
+
+enum ConsonantStyle {
+    case basic
+    case asperated
+    case tensed
+}
+
+struct Consonoant {
+    let character: Character
+    let style: ConsonantStyle
+}
+
+private extension Character {
+    static let basicKVowels: [Character] = [
+        "\u{1161}",
+        "\u{1165}",
+        "\u{1169}",
+        "\u{116E}",
+        "\u{1173}",
+        "\u{1175}"
+    ]
+}
+
+extension Consonoant {
+    var basicVowels: [Character] {
+        Character.basicKVowels.map {
+            Character("\(self.character)\($0)")
+        }
+    }
+}
